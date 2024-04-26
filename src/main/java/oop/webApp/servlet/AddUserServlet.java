dc1c484706bae7a7931a4ea940fe944fc3b8e790
John An g <john.ang@stengg.com>
2024-04-26 22:08:18 +0800

    changes

--- src/main/java/oop/webApp/servlet/AddUserServlet.java
+++ src/main/java/oop/webApp/servlet/AddUserServlet.java
@@ -41,21 +41,6 @@ public class AddUserServlet extends HttpServlet {
 		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/index.jsp");
 		requestDispatcher.forward(request, response);
 		
-//		RegisterDeo registerDeo = new RegisterDeo();
-//
-//		String userRegistered = null;
-//		try {
-//			userRegistered = registerDeo.registerUser(registerBean);
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//		if(userRegistered.equals("SUCCESS")) {
-//			request.setAttribute("user", registerBean);
-//			RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/user.jsp");
-//			requestDispatcher.forward(request, response);
-//		}else {  //On Failure, display a message to the User.
-//			request.setAttribute("errMessage", userRegistered);
-//			request.getRequestDispatcher("signup.jsp").forward(request, response);
-//		}	
+
 	}	
 }
