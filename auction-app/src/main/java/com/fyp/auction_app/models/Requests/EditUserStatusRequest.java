27d25d786d2dd732cc03b38ecb3927ef1c7cc8f1
Jeffrey Chong <jeffrey.chong28@gmail.com>
2024-07-30 21:50:31 +0800

    feat: add amin features

---
+++ auction-app/src/main/java/com/fyp/auction_app/models/Requests/EditUserStatusRequest.java
@@ -0,0 +1,16 @@
+package com.fyp.auction_app.models.Requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuspendUserRequest {
+
+    private String username;
+
+}
